08c27b45b7a8831f8854e69e2b4e8fc176aa84f3
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-09-11 23:42:55 +0300

    [UPDATE] Django Templates Works

--- pages/views.py
+++ pages/views.py
@@ -5,17 +5,20 @@ from django.shortcuts import render
 
 
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
-    print(request.user)
-    return HttpResponse("<h1>Hello World!</h1>")
+    return render(request, "home.html", {})
 
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+    return render(request, "contact.html", {})
 
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse("<h1>About Page</h1>")
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [1432, 2423, 'fasf', 4423, 'dsg']
+    }
+    return render(request, "about.html", my_context)
 
 
 def social_view(request, *args, **kwargs):
