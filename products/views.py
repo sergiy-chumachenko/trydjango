81ac56c76d8d2a4c4b43a74efafddfe78a97995f
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2018-09-25 23:18:37 +0300

    [UPDATE] Handle Dynamic URLs + DoesNotExists Error

--- products/views.py
+++ products/views.py
@@ -1,23 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from . models import Product
 from . forms import ProductForm, RawProductForm
 
 
-# Create your views here.
+def dynamic_lookup_view(request, my_id):
+    # obj = Product.objects.get(id=my_id)
+    obj = get_object_or_404(Product, id=my_id)
+    # try:
+    #     obj = Product.objects.get(id=my_id)
+    # except Product.DoesNotExist:
+    #     raise Http404
+    context = {
+        'object': obj
+    }
+    return render(request, 'products/product_detail.html', context)
+
+
 def product_create_view(request):
     initial_data = {
         "title": "My aewesome title",
 
     }
     obj = Product.objects.get(id=1)
-    form = ProductForm(request.POST or None, instance=obj)
+    form = ProductForm(request.POST or None, initial=initial_data, instance=obj)
     if form.is_valid():
         form.save()
     context = {
         'form': form
     }
     return render(request, "products/product_create.html", context)
+
+
 # def product_create_view(request):
 #     form = RawProductForm()
 #     if request.method == 'POST':
@@ -31,6 +46,7 @@ def product_create_view(request):
 #         'form': form
 #     }
 #     return render(request, "products/product_create.html", context)
+
 
 # def product_create_view(request):
 #     form = ProductForm(request.POST or None)
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -17,13 +17,16 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, about_view
-from products.views import product_detail_view, product_create_view
+from products.views import  (product_detail_view,
+                             product_create_view,
+                             dynamic_lookup_view)
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('about', about_view),
     path('contacts', contact_view),
     path('products', product_detail_view),
+    path('products/<int:my_id>/', dynamic_lookup_view, name='product'),
     path('create', product_create_view),
     path('admin', admin.site.urls),
 ]
