58e09aeca3da1452c6c097272f320d1709d66b6e
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2018-09-29 13:09:11 +0300

    [UPDATE] Render a QuerySet

--- products/views.py
+++ products/views.py
@@ -5,6 +5,14 @@ from . models import Product
 from . forms import ProductForm, RawProductForm
 
 
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, 'products/product_list.html', context)
+
+
 def dynamic_lookup_view(request, my_id):
     # obj = Product.objects.get(id=my_id)
     obj = get_object_or_404(Product, id=my_id)
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -1,32 +1,17 @@
-"""trydjango URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, about_view
-from products.views import  (product_detail_view,
-                             product_create_view,
-                             dynamic_lookup_view,
-                             product_delete_view)
+from products.views import (product_create_view,
+                            dynamic_lookup_view,
+                            product_delete_view,
+                            product_list_view)
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('about', about_view),
     path('contacts', contact_view),
-    path('products', product_detail_view),
+    path('products', product_list_view, name='product_list'),
     path('products/<int:my_id>/delete/', product_delete_view, name='product-delete'),
     path('products/<int:my_id>/', dynamic_lookup_view, name='product'),
     path('create', product_create_view),
