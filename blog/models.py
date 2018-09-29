536ffa611f957e432d6a4fcec260c3cfcc921b5b
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2018-09-29 16:59:13 +0300

    [UPDATE] Class Based Views - DetailView

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 class Article(models.Model):
     title = models.CharField(max_length=50, blank=False, null=False)
     text = models.TextField(max_length=500, blank=False, null=False)
+
+    def get_absolute_url(self):
+        return reverse('articles:article-detail', kwargs={"id": self.id})
--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from blog.views import (ArticleListView)
+from blog.views import (ArticleListView,
+                        ArticleDetailView)
 
 app_name = 'articles'
 urlpatterns = [
     path('', ArticleListView.as_view(),  name='article-list'),
+    path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
     # path('<int:my_id>/delete/', product_delete_view, name='product-delete'),
-    # path('<int:my_id>/', product_detail_view, name='product-detail'),
+
     # path('<int:my_id>/update/', product_update_view, name='product-update'),
     # path('create', product_create_view, name='product-create')
 ]
