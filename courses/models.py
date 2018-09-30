10c4597356912c2a191111172ff91409c04cc411
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2018-09-30 17:02:40 +0300

    [UPDATE] Raw List Class Based View

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 class Course(models.Model):
     title = models.CharField(max_length=50, blank=False, null=False)
+
+    def get_absolute_url(self):
+        return reverse('courses:course-detail', kwargs={'id': self.id})
--- courses/urls.py
+++ courses/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from courses.views import (
     CourseView,
+    CourseListView
     # my_fbv
 )
 
 app_name = 'courses'
 urlpatterns = [
-    path('', CourseView.as_view(template_name='contact.html'),  name='courses-list'),
-    path('<int:id>/', CourseView.as_view(), name='article-detail'),
+    path('', CourseListView.as_view(),  name='courses-list'),
+    path('<int:id>/', CourseView.as_view(), name='course-detail'),
     # path('create/', ArticleCreateView.as_view(), name='article-create'),
     # path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
     # path('<int:id>/delete/', ArticleDeleteView.as_view(), name='article-delete')
